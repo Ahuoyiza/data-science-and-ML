a4b35858dc076bca4ff2ed930259255f96c35006
Sophia Ahuoyiza Abubakar <60071500+Ahuoyiza@users.noreply.github.com>
2023-07-27 00:24:55 +0100

    Update basic-sql-examples.sql

--- intro-data-science/basic-sql-examples.sql
+++ intro-data-science/basic-sql-examples.sql
@@ -47,3 +47,65 @@ ORDER BY account_id, total_amt_usd DESC;
 SELECT id, account_id, total_amt_usd
 FROM orders
 ORDER BY total_amt_usd DESC, account_id ;
+
+-- 6. Write a query that:
+
+-- Pulls the first 5 rows and all columns from the orders table that have a dollar amount of gloss_amt_usd greater than or equal to 1000.
+
+-- Pulls the first 10 rows and all columns from the orders table that have a total_amt_usd less than 500.
+SELECT *
+FROM orders
+WHERE gloss_amt_usd >= 1000
+LIMIT 5;
+-----
+SELECT *
+FROM orders
+WHERE total_amt_usd < 500
+LIMIT 10;
+
+-- 7. Filter the accounts table to include the company name, website, and the primary point of
+--   contact (primary_poc) just for the Exxon Mobil company in the accounts table.
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil';
+
+-- Using the orders table:
+
+-- 8. Create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for each order. L
+--   imit the results to the first 10 orders, and include the id and account_id fields.
+
+
+-- 9. Write a query that finds the percentage of revenue that comes from poster paper for each order. 
+--   You will need to use only the columns that end with _usd. (Try to do this without using the total column.) 
+--   Display the id and account_id fields also. NOTE - you will receive an error with the correct solution to this question. 
+--   This occurs because at least one of the values in the data creates a division by zero in your formula. You will learn later in the 
+--   course how to fully handle this issue. For now, you can just limit your calculations 
+--   to the first 10 orders, as we did in question #1, and you'll avoid that set of data that causes the problem.
+
+SELECT id,account_id, standard_amt_usd/standard_qty AS unit_price
+FROM orders
+LIMIT 10;
+----
+SELECT id,account_id,  
+(poster_amt_usd/(standard_amt_usd+poster_amt_usd+gloss_amt_usd)) * 100 AS percent_poster
+FROM orders
+LIMIT 10;
+
+-- 10. Use the accounts table to find
+
+-- All the companies whose names start with 'C'.
+
+-- All companies whose names contain the string 'one' somewhere in the name.
+
+-- All companies whose names end with 's'.
+SELECT name
+FROM accounts
+WHERE name LIKE 'C%';
+---
+SELECT name
+FROM accounts
+WHERE name LIKE '%one%';
+-----
+SELECT name
+FROM accounts
+WHERE name LIKE '%s';
